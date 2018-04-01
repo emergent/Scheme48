51a66a1fce439dc47bfcc2dc997095f038706008
emergent <yosikawa@gmail.com>
2018-04-01 22:56:23 +0900

    add Reader

---
+++ src/Reader.hs
@@ -0,0 +1,15 @@
+module Reader (readExpr) where
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value: " ++ show val
