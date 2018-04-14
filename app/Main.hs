2f4a7f71738f7d293adb184e2a0089641364d011
emergent <yosikawa@gmail.com>
2018-04-15 01:42:32 +0900

    add repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,39 @@
 module Main where
 
-import System.Environment
-import Control.Monad
-import Reader
-import Eval
-import Error
+import           Control.Monad
+import           Error
+import           Eval
+import           Reader
+import           System.Environment
+import           System.IO
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+    result <- prompt
+    if pred result
+        then return ()
+        else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
 
 main :: IO ()
-main = do -- getArgs >>= print . eval . readExpr . head
-    (a:_) <- getArgs
-    let evaled = liftM show $ readExpr a >>= eval
-    putStrLn $ extractValue $ trapError evaled
+main = do
+    args <- getArgs
+    case length args of
+        0         -> runRepl
+        1         -> evalAndPrint $ args !! 0
+        otherwise -> putStrLn "Program takes only 0 or 1 argument"
 
